3ded0ebaf9cb2d081596c1fe08fb7556a4a7265c
unknown <cawatitov@gmail.com>
2018-05-14 19:47:12 +0300

    bla

--- src/GameState.java
+++ src/GameState.java
@@ -2,15 +2,18 @@ import java.awt.*;
 
 public class GameState extends State{
     private Player player;
+    private Enemy enemy;
 
     public GameState(Game game){
         super(game);
         player = new Player(game,200,game.getHeight() - Assets.player.getHeight());
+        enemy = new Enemy(game,100,70);
     }
 
     @Override
     public void update() {
         player.update();
+        enemy.update();
         if(player.isShotMade())
             player.getChain().update();
     }
@@ -18,6 +21,7 @@ public class GameState extends State{
     @Override
     public void draw(Graphics g) {
         player.draw(g);
+        enemy.draw(g);
         if(player.isShotMade())
             player.getChain().draw(g);
     }
