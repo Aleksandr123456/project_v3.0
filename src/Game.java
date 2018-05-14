35661e9a5613b00c4a2d3bfaad257ca6cf3889df
unknown <cawatitov@gmail.com>
2018-05-14 19:17:51 +0300

    suka

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,13 @@
+import javafx.scene.shape.Circle;
+
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
-public class Game implements Runnable{
+public class Game implements Runnable,ActionListener{
 
     private Display display;
     private Thread thread;
@@ -19,6 +24,8 @@ public class Game implements Runnable{
     private SpriteSheet sheet;
 
     int x = 0;
+    int y = 0;
+
 
     public Game(String title, int width, int height){
         this.width = width;
@@ -51,7 +58,14 @@ public class Game implements Runnable{
         //Draw here
 
         g.drawImage(Assets.background,0 ,0 ,null);
-        g.drawImage(Assets.player,x,10, null);
+        g.drawImage(Assets.player,x,y, null);
+
+        Circle c = new Circle();
+        c.setCenterX(20);
+        c.setCenterY(50);
+        c.setRadius(20);
+
+
 
         //End drawing
         bs.show();
@@ -68,15 +82,26 @@ public class Game implements Runnable{
         long now;
         long lastTime = System.nanoTime();
 
+        long timer = 0;
+        int ticks = 0;
 
         while (running) {
             now = System.nanoTime();
             delta += (now - lastTime) / timePerUpdate;
+            timer += now - lastTime;
             lastTime = now;
 
             if (delta >= 1) {
                 update();
                 render();
+                ticks++;
+                delta--;
+            }
+
+            if(timer >= 1000000000){
+                System.out.println(" " +ticks);
+                ticks = 0;
+                timer = 0;
             }
         }
 
@@ -103,4 +128,8 @@ public class Game implements Runnable{
         }
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
